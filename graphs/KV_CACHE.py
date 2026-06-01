7064d3eb4ef9ae088d229e0c8f6e46330247c290
Eliezer-Carvalho <eliezer.carvalho.11.8a@gmail.com>
2026-06-01 13:26:04 +0100

    main

--- graphs/KV_CACHE.py
+++ graphs/KV_CACHE.py
@@ -27,17 +27,17 @@ def KV_CACHE_TRADICIONAL (num_layers, context_length, hidden_size, bytes_per_val
 #KV cache é um dos maiores problemas de inferência em LLMs long-context.
 #Por isso surgiram várias arquiteturas para tentar atacar este problema de maneiras diferentes.
 
-def KV_CACHE_GQA (num_layers, context_length, kv_heads, head_dim, bytes_per_value):
+def KV_CACHE_GQA (batch_size, num_layers, context_length, kv_heads, head_dim, bytes_per_value):
 #Estes parâmetros podem vir em nomes bastante diferentes
 #Context_Length pode ser vários valores mas nunca superiores ao contexto máximo do modelo
 #Head_dim = Hidden_Size // num_attention_heads
 
-    #KV_CACHE = humanize.naturalsize (2 * num_layers * context_length * kv_heads * head_dim * bytes_per_value)
-    KV_CACHE = 2 * num_layers * context_length * kv_heads * head_dim * bytes_per_value
+    KV_CACHE = humanize.naturalsize (2 * batch_size * num_layers * context_length * kv_heads * head_dim * bytes_per_value)
+    #KV_CACHE = 2 * batch_size * num_layers * context_length * kv_heads * head_dim * bytes_per_value
     
     return KV_CACHE
 
 
 #Exemplo:
-    #contexto_max = KV_CACHE_GQA (36, 32000, 8, 128, 2)
+    #contexto_max = KV_CACHE_GQA (1, 36, 2000, 8, 128, 2)
     #print (f"KV_CACHE_MAX do modelo Qwen3-4B-Base = {contexto_max}")
