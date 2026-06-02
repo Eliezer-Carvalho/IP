0bcab9d0c9a0d485ca6040cf9a5f463a26471573
Eliezer-Carvalho <eliezer.carvalho.11.8a@gmail.com>
2026-06-02 09:23:53 +0100

    main

---
+++ Week 1/quant.py
@@ -0,0 +1,18 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+
+MODELO_PATH = r"C:\Users\Admin\Desktop\models\Teste\Qwen3-4B-Base\BASE"
+
+
+quantization = BitsAndBytesConfig (
+    load_in_4bit = True,  #Quantização
+    bnb_4bit_quant_type = 'nf4',  #Tipo de Quantização
+    bnb_4bit_use_double_quant = True, #Double Quantization
+    bnb_4bit_compute_dtype = 'float16', #Tipo de precisão usada nos cálculos durante a inferência.
+)
+
+modelo = AutoModelForCausalLM.from_pretrained (MODELO_PATH, device_map = 'auto', quantization_config = quantization)
+
+tokenizer = AutoTokenizer.from_pretrained (MODELO_PATH)
+
+modelo.save_pretrained ('/models')
+tokenizer.save_pretrained ('/models')
