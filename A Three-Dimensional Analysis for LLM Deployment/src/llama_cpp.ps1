c4fdfad8927668aa8dd4ed13693286289c08b967
Eliezer Carvalho <eliezer.carvalho.11.8a@gmail.com>
2026-07-31 13:15:49 +0100

    Update llama_cpp.ps1

--- A Three-Dimensional Analysis for LLM Deployment/src/llama_cpp.ps1
+++ A Three-Dimensional Analysis for LLM Deployment/src/llama_cpp.ps1
@@ -2,12 +2,12 @@
 convert_hf_to_gguf.py "file_path" --outfile x.gguf
 python .\convert_hf_to_gguf.py "C:\Users\Admin\Desktop\models\amalia-llmAMALIA-9B-0626-DPO\Original" --outfile "C:\Users\Admin\Desktop\models\amalia-llmAMALIA-9B-0626-DPO\GGUF\amalia-bf16.gguf" --outtype bf16
 
-
 #métricas de benchmark
 llama-bench -m "file_path" -p 2000 -b 1024 -ngl 0 -n 50 -o json
 
 #quantização do modelo
 llama-quantize "file_path" "path_para_guardar_e_nome" q4_k_S
+llama-quantize "C:\Users\Admin\Desktop\models\amalia-llmAMALIA-9B-0626-DPO\GGUF\amalia-bf16.gguf" "C:\Users\Admin\Desktop\models\amalia-llmAMALIA-9B-0626-DPO\ggufq\amalia_q2K" Q2_K
 #https://github.com/ggml-org/llama.cpp/blob/master/tools/quantize/README.md
 #https://github.com/ggml-org/llama.cpp/pull/1684
 
